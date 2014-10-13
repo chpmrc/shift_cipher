f2da59be0035a0c4fad675d2f76fcfe28fbaccb7
mardurhack <mardurhack@gmail.com>
2014-10-13 22:53:17 +0300

    Fix break operation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Shift Cipher
 
-This program breaks a given (shift) ciphertext by using a dictionary attack. It supports spurious keys (i.e. two or more keys that lead to the same ciphertext for two or more different plaintexts).
+This program breaks a given (shift) ciphertext by using a bruteforce attack and comparing words with a given dictionary. It supports spurious keys (i.e. two or more keys that lead to the same ciphertext for two or more different plaintexts).
 
 ## Usage
 
@@ -8,8 +8,10 @@ Must have python interpreter installed.
 
 From a terminal:
 
-`python shift_cipher.py path_to_ciphertext path_to_dictionary`
+`python shift_cipher.py path_to_ciphertext path_to_dictionary path_to_resulting_plaintext`
 
 The output is a list of tuples. Each tuple contains the corresponding plaintext and the related key.
 
 For example the following list is returned for the ciphertext `bsfob`: `[('river', '16'), ('arena', '25')]`
+
+__Note__: since every attempt is matched against a big dictionary (the given one contains ~300000 words) the execution might takes some time.
--- shift_cipher.py
+++ shift_cipher.py
@@ -4,41 +4,98 @@ Simple implementation of a shift cipher breaker (supports spurious keys, uses di
 
 import sys
 
+def find_spurious_keys(n, dictionary, output_file):
+	""" Find (if any) at most n spurious keys using the given dictionary """
+	spurious = []
+	min_length = 5
+	for w in dictionary:
+		for k in xrange(1, 26):
+			# Exclude short words
+			if len(w) < min_length:
+				continue
+			# if w.index("river") == 0:
+			shifted_w = shift(w, k)
+			for subk in xrange(1, 26):
+				for subw in dictionary:
+					if len(subw) >= min_length and subw != w:
+						# if subw.index("arena") == 0:
+						shifted_subw = shift(subw, k)
+						# print "Using %s (%d) -> %s , %s (%d) -> %s\n" % (w, k, shifted_w, subw, subk, shifted_subw)
+						if (shifted_subw == shifted_w):
+							tupl = (w, shifted_w, k, subw, shifted_subw, subk)
+							spurious.append(tupl)
+							output_file.write(str(tupl))
+							output_file.flush()
+
+						if len(spurious) >= n:
+							break
+	return spurious
+
+
+def shift(text, key):
+	shifted = ""
+	for i in xrange(0, len(text)):
+		shifted += chr((((ord(text[i]) - 97) + key) % 26) + 97)
+	return shifted
+
+def find_word(text, dictionary):
+	# print "Looking for " + text + "..."
+	for w in dictionary:
+		if text == w:
+			# print "Found! " + w
+			return True
+	return False
+
 def break_ciphertext(ciphertext, dictionary):
 	plaintexts = []
 	cur_word = ""
 
-	print ciphertext
-
 	for k in xrange(0, 26):
 		# Shift with the key
-		for i in xrange(0, len(ciphertext)):
-			# Let's remember to map from alphabet indices to ASCII code!
-			cur_word += chr((((ord(ciphertext[i]) - 97) + k) % 26) + 97)
-		try:
-			dictionary.index(cur_word + "\n")
+		cur_word = shift(ciphertext, k) 
+		if find_word(cur_word, dictionary):
 			plaintexts.append((cur_word, str(k)))
-		except Exception:
-			pass
 		cur_word = ""
 	return plaintexts
 
-def print_usage(name):
-	print "Usage: " + name + " path_to_ciphertext path_to_dictionary"
+def print_usage_break(name):
+	print "Usage: " + name + " break path_to_dictionary path_to_resulting_plaintext path_to_ciphertext"
+
+def print_usage_findspurious(name):
+	print "Usage: " + name + " spurious path_to_dictionary path_to_dump_file"
 
 def main(args):
-	if (len(args) != 3):
-		print_usage(args[0])
+	if (len(args) < 2):
+		print_usage_break(args[0])
+		print_usage_findspurious(args[0])
 		exit()
-	ciphertext_file = args[1]
+	operation = args[1]
 	dictionary_file = args[2]
-	ciphertext = "".join(open(args[1], "r").readlines())
-	dictionary = open(args[2], "r").readlines()
-	for l in dictionary:
-		l = l.strip()
-	# print dictionary
-	plaintexts = break_ciphertext(ciphertext, dictionary)
-	print plaintexts
+	dictionary = open(dictionary_file, "r").readlines()
+	ciphertext_file = None
+	ciphertext = None
+	output_file = args[3]
+	output_file_obj = open(output_file, "w", 0)
+	# Remove all whitespaces from the dictionary
+	for i in xrange(0, len(dictionary)):
+		dictionary[i] = dictionary[i].strip()
+
+	if operation == "break":
+		if len(args) < 5:
+			print_usage_break(args[0])
+			exit()
+		ciphertext_file = args[4]
+		ciphertext = "".join(open(ciphertext_file, "r").readlines())
+		plaintexts = break_ciphertext(ciphertext, dictionary)
+		output_file_obj.write(str(plaintexts))
+	if operation == "spurious":
+		if len(args) < 3:
+			print_usage_findspurious(args[0])
+			exit()
+		spurious_keys = find_spurious_keys(3, dictionary, output_file)
+		
+	output_file_obj.close()	
+
 
 
 main(sys.argv)
